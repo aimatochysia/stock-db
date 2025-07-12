924f67f2067a1437b54e9f892fe1e8f59cdfffa3
aimatochysia <petra.michael@outlook.com>
2025-07-12 12:14:26 +0700

    added backend server for getter

---
+++ api/index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+
+//get only one
+app.get('/api/daily/:symbol', (req, res) => {
+  const symbol = req.params.symbol
+  const filePath = path.join(__dirname, '../data', `${symbol}.json`)
+
+  if (!fs.existsSync(filePath))
+    return res.status(404).json({ error: 'Not found' })
+
+  const json = fs.readFileSync(filePath)
+  res.setHeader('Content-Type', 'application/json')
+  res.send(json)
+})
+
+//get all
+app.get('/api/daily', (req, res) => {
+  const folderPath = path.join(__dirname, '../data')
+  const files = fs.readdirSync(folderPath).filter(f => f.endsWith('.json'))
+
+  const result = files.map(f => {
+    const content = fs.readFileSync(path.join(folderPath, f))
+    return { [f.replace('.json', '')]: JSON.parse(content) }
+  })
+
+  res.json(Object.assign({}, ...result))
+})
+
+module.exports = app
