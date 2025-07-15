d0b528c758e16a446a6dbeb9662c6044052af623
Petra Michael <petraxmichael@gmail.com>
2025-07-15 10:44:22 +0700

    added test function to work both on local dev and deployment

--- api/index.js
+++ api/index.js
@@ -32,4 +32,11 @@ app.get('/api/daily', (req, res) => {
   res.json(Object.assign({}, ...result))
 })
 
+if (require.main === module) {
+  const PORT = process.env.PORT || 3000
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+  })
+}
+
 module.exports = app
